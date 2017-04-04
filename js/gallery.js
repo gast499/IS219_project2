ea1709eed023e2c23bec66df779040eacea2e19e
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2017-04-03 23:50:11 -0400

    bugtesting arrows

--- js/gallery.js
+++ js/gallery.js
@@ -150,7 +150,7 @@ $(document).ready( function() {
     /*if (mCurrentIndex  === 0){
       mCurrentIndex = mImages.length-1;
     }*/
-    mCurrentIndex = mCurrentIndex - 1;
+    mCurrentIndex = mCurrentIndex - 2;
     swapPhoto();
   });
 });
